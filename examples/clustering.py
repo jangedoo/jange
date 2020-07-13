f55c29752d9473e2d289e662d97d04b13debc233
Sanjaya Subedi <sanjaya.subedi@serviceware.se>
2020-07-13 15:10:13 +0200

    added clustering example

---
+++ examples/clustering.py
@@ -0,0 +1,32 @@
+# %%
+from sklearn.datasets import fetch_20newsgroups
+import spacy
+from jange import ops, stream, vis
+
+data = fetch_20newsgroups(shuffle=False)
+n = 1000
+texts = data["data"][:n]
+label_names = dict((enumerate(data["target_names"])))
+labels = [label_names[i] for i in data["target"][:n]]
+
+# %%
+nlp = spacy.load("en_core_web_md")
+# %% Extract features
+input_ds = stream.DataStream(items=texts, context=labels)
+features_ds = input_ds.apply(
+    ops.text.remove_emails(nlp),
+    ops.text.remove_links(nlp),
+    ops.text.tfidf(nlp, max_features=5000),
+)
+
+# %% Extract clusters
+clusters_ds = features_ds.apply(ops.cluster.minibatch_kmeans(n_clusters=4))
+print(len(clusters_ds.items))
+
+# %% Visualization
+reduced_features = features_ds.apply(ops.dim.pca(n_dim=3))
+vis.cluster.visualize(reduced_features, clusters_ds)
+
+
+# %%
+
