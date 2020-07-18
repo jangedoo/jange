f050673cc49101471b82ee221097075b70f2002f
= <jangedoo@gmail.com>
2020-07-18 10:38:58 +0200

    added test for document embedding

---
+++ tests/ops/text/vectorize/test_doc_embedding.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pytest
+
+from jange import ops, stream
+
+
+def test_returns_a_stream_with_doc_vectors():
+    ds = stream.DataStream(["this is", "another", "sentence"])
+
+    vector_ds = ds.apply(ops.text.doc_embedding())
+    assert vector_ds.total_items == ds.total_items
+    assert vector_ds.context is not None
+    assert isinstance(vector_ds.items, np.ndarray)
